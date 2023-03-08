8d8778ce9c074719c2c54062dee441b77322f994
reload0406 <reload0406@gmail.com>
2023-03-08 16:42:42 +0900

    chapter_01

---
+++ 백준/단계별로/chapter_01/BOJ_18108.java
@@ -0,0 +1,14 @@
+package 단계별로.chapter_01;
+/*https://www.acmicpc.net/problem/18108*/
+
+import java.util.Scanner;
+
+public class BOJ_18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        System.out.println(year - 543);
+    }
+}
