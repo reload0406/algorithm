deda1f41dab53c96619203ab5ba6ba1c9c5bfabd
reload0406 <reload0406@gmail.com>
2023-03-08 15:55:55 +0900

    chapter_01

---
+++ 백준/단계별로/chapter_01/BOJ_1000.java
@@ -0,0 +1,17 @@
+package 단계별로.chapter_01;
+
+/* https://www.acmicpc.net/problem/1000 */
+
+import java.util.Scanner;
+
+public class BOJ_1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.print(a + b);
+
+    }
+}
