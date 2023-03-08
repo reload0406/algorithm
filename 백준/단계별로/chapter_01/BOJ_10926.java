ba80f3d9e5ffc7aad1b7e7ccb7c770867c42f366
reload0406 <reload0406@gmail.com>
2023-03-08 16:41:59 +0900

    chapter_01

---
+++ 백준/단계별로/chapter_01/BOJ_10926.java
@@ -0,0 +1,15 @@
+package 단계별로.chapter_01;
+/* https://www.acmicpc.net/problem/10926 */
+
+import java.util.Scanner;
+
+public class BOJ_10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String id = sc.nextLine();
+
+        System.out.println(id + "??!");
+
+    }
+}
