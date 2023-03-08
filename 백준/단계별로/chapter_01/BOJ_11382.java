4dec62bdf1816cb5abffbb563d855ffccb0b3d3e
reload0406 <reload0406@gmail.com>
2023-03-08 16:38:01 +0900

    chapter_01

---
+++ 백준/단계별로/chapter_01/BOJ_11382.java
@@ -0,0 +1,17 @@
+package 단계별로.chapter_01;
+/* https://www.acmicpc.net/problem/11382 */
+/* 왜 이 문제만 long 을 사용해야하는가 질문 */
+import java.util.Scanner;
+
+public class BOJ_11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+
+        System.out.println(a + b + c);
+    }
+}
