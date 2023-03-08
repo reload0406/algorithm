1c28801821efdda96ec56656ba1560beab90aada
reload0406 <reload0406@gmail.com>
2023-03-08 16:43:40 +0900

    chapter_01

---
+++ 백준/단계별로/chapter_01/BOJ_1008.java
@@ -0,0 +1,17 @@
+package 단계별로.chapter_01;
+/* https://www.acmicpc.net/problem/1008 */
+
+import java.util.Scanner;
+
+public class BOJ_1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        double result = (double) a / b;
+
+        System.out.print(result);
+
+    }
+}
