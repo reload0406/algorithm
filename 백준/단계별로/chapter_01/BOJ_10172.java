41c55b9f8fce3bff9de6e250aa0218599910b915
reload0406 <reload0406@gmail.com>
2023-03-08 16:41:19 +0900

    chapter_01

---
+++ 백준/단계별로/chapter_01/BOJ_10172.java
@@ -0,0 +1,13 @@
+package 단계별로.chapter_01;
+
+/* https://www.acmicpc.net/problem/10172 */
+public class BOJ_10172 {
+    public static void main(String[] args) {
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
