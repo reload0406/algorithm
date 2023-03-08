34a58ee2a70be8a1b656743fa550a4a1ed9ed58c
reload0406 <reload0406@gmail.com>
2023-03-08 16:45:58 +0900

    chapter-01

---
+++ 백준/단계별로/chapter_01/BOJ_2557.java
@@ -0,0 +1,7 @@
+package 단계별로.chapter_01;
+/* https://www.acmicpc.net/problem/2557 */
+public class BOJ_2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
