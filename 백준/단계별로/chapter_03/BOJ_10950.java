b5191064c3ebd7b5138704576b80d785d189abd5
reload0406 <reload0406@gmail.com>
2023-03-10 20:16:21 +0900

    chapter_03

---
+++ 백준/단계별로/chapter_03/BOJ_10950.java
@@ -0,0 +1,21 @@
+package 단계별로.chapter_03;
+/* https://www.acmicpc.net/problem/10950 */
+
+import java.util.Scanner;
+
+public class BOJ_10950 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println(a + b);
+        }
+    }
+}
